706b49f7494467e1c9ef2b2a5a91ac6f9efb6d8d
Tauri StClaire <tauristclaire@gmail.com>
2022-11-28 20:54:03 +0000

    Creates blank note to fill and added click events/handlers

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ const Header = (props) => (
     <header className="app-header__title">
         <h1>Super Sticky Notes</h1>
         <aside className="app-header__controls">
-            <button className="add-new">+ New Note</button>
+            <button className="add-new" onCLick={props.addNote} >+ New Note</button>
             <input className="search" type="text" placeholder="Type here to search..." value={props.searchText} />
         </aside>
     </header>
