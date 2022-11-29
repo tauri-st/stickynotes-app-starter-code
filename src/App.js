2010ebaadb1a638bc6029b1af96cfc022dfa6fcc
Tauri StClaire <tauristclaire@gmail.com>
2022-11-29 01:15:51 +0000

    Mapped over and passed props to allow title and description on note to be updated

--- src/App.js
+++ src/App.js
@@ -26,12 +26,34 @@ class App extends Component {
     //add the new note to existing notes array in State
     this.setState({ notes: [newNote, ...this.state.notes] });
   };
+  
+  onType = (editMeId, updatedKey, updatedValue) => {
+    //editMeId == id of the note that is edited
+    //updatedKey == title or description field?
+    //updatedValue == value of title or description
+    
+    const updatedNotes = this.state.notes.map( note =>
+      if (note.id !== editMeId){
+        return note;
+      }
+      else {
+        if (updatedKey === "title"){
+          note.title = updatedValue;
+          return note;
+        }
+        else {
+          note.description = updatedValue;
+          return note;
+        }
+      });
+    this.setState({ notes: updatedNotes })
+  };
 
   render() {
     return (
       <div>
         <Header searchText={this.state.searchText} addNote={this.addNote} />
-        <NotesList notes={this.state.notes} />
+        <NotesList onType={this.onType} notes={this.state.notes} />
       </div>
     );
   }
