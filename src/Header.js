6e8c4059e769bdc01fc07bddaecaaa0dcc39ff6d
Tauri StClaire <tauristclaire@gmail.com>
2022-11-30 01:36:21 +0000

    Added search function

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,26 @@
 import React from "react";
 
-const Header = (props) => (
+const Header = (props) => {
+  const callSearch = (e) => {
+    props.onSearch(e.target.value);
+  };
+  return (
     <header className="app-header__title">
-        <h1>Super Sticky Notes</h1>
-        <aside className="app-header__controls">
-            <button className="add-new" onCLick={props.addNote} >+ New Note</button>
-            <input className="search" type="text" placeholder="Type here to search..." value={props.searchText} />
-        </aside>
+      <h1>Super Sticky Notes</h1>
+      <aside className="app-header__controls">
+        <button className="add-new" onCLick={props.addNote}>
+          + New Note
+        </button>
+        <input
+          className="search"
+          type="text"
+          placeholder="Type here to search..."
+          value={props.searchText}
+          onChange={callSearch}
+        />
+      </aside>
     </header>
-);
+  );
+};
 
 export default Header;
